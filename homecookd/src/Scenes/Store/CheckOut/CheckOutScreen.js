a3569949df6a774e40f6d7cb38196a72ec955c20
IrieY56 <IrieY56@gmail.com>
2018-04-23 16:34:29 -0400

    Check Out button now creates a order

--- homecookd/src/Scenes/Store/CheckOut/CheckOutScreen.js
+++ homecookd/src/Scenes/Store/CheckOut/CheckOutScreen.js
@@ -3,17 +3,36 @@ import { Button, Modal, Header, Image, Item } from 'semantic-ui-react';
 import CheckOut from './CheckOut';
 import CheckOutItem from './CheckOutItem';
 import { connect } from 'react-redux';
+import { createOrder } from '../../../Utils/storeData';
+import history from '../../../Utils/history'
 
 class CheckOutScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      user_api_token: ''
     };
+
+    this.getUserApiToken = this.getUserApiToken.bind(this);
+    this.checkOut = this.checkOut.bind(this);
+  }
+  getUserApiToken() {
+    if(localStorage.getItem('api_token') !== null) {
+      return localStorage.getItem('api_token');
+    } else {
+      // redirect to login page
+      history.push('/Auth/Login');
+    }
+  }
+
+  checkOut() {
+    let user_api_token = this.getUserApiToken();
+    let seller_id = this.props.seller_id;
+    let foodItems = this.props.cart.cart;
+    createOrder(user_api_token, seller_id, foodItems);
   }
 
   state = { open: false }
-
   show = size => () => this.setState({ size, open: true })
   close = () => this.setState({ open: false })
 
@@ -21,7 +40,6 @@ class CheckOutScreen extends Component {
   render() {
 
     return (
-
       <Modal open={this.props.open} >
         <Modal.Content scrolling>
           <Item.Group relaxed divided unstackable>
@@ -36,7 +54,7 @@ class CheckOutScreen extends Component {
           <Button onClick={this.props.handleClose} negative>
             Exit
           </Button>
-          <Button positive icon='checkmark' labelPosition='right' content='Check Out' />
+          <Button onClick={this.checkOut} positive icon='checkmark' labelPosition='right' content='Check Out' />
         </Modal.Actions>
       </Modal>
     )
